3234fcdd562c38f4aaaa9e960efff0747e325c45
Jake Soenneker <jake@soenneker.com>
2025-08-16 19:34:39 -0500

    constants

--- src/FailedRequestLoggingMiddleware.cs
+++ src/FailedRequestLoggingMiddleware.cs
@@ -5,6 +5,7 @@ using Soenneker.Extensions.HttpRequests;
 using Soenneker.Extensions.ValueTask;
 using Soenneker.Middlewares.FailedRequestLogging.Abstract;
 using System.Threading.Tasks;
+using Soenneker.Constants.Apis;
 
 namespace Soenneker.Middlewares.FailedRequestLogging;
 
@@ -13,8 +14,6 @@ public sealed class FailedRequestLoggingMiddleware : IFailedRequestLoggingMiddle
 {
     private readonly RequestDelegate _next;
     private readonly ILogger<FailedRequestLoggingMiddleware> _logger;
-
-    private const string _controllerHitKey = "ControllerHit";
 
     public FailedRequestLoggingMiddleware(RequestDelegate next, ILogger<FailedRequestLoggingMiddleware> logger)
     {
@@ -28,14 +27,14 @@ public sealed class FailedRequestLoggingMiddleware : IFailedRequestLoggingMiddle
 
         await _next(context);
 
-        if (context.Items.ContainsKey(_controllerHitKey))
+        if (context.Items.ContainsKey(ApiConstants.ControllerHitFlag))
             return;
 
         int statusCode = context.Response.StatusCode;
 
         if (statusCode is StatusCodes.Status400BadRequest or StatusCodes.Status404NotFound or StatusCodes.Status405MethodNotAllowed)
         {
-            string headers = context.Request.Headers.SerializeHeadersJson();
+            string headers = context.Request.Headers.ToJsonString();
             string? body = await context.Request.ReadBody().NoSync();
 
             _logger.LogWarning("Request {Method} {Path} failed with status code {StatusCode}. Headers: {Headers} Body: {Body}", context.Request.Method,
